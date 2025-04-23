5639669bb33cb99e56c8e4a85f45b84927a0bf3a
Rashid <rashidsaifi604@gmail.com>
2025-04-23 23:18:07 +0530

    github pages configuration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   site: 'https://nabiya-enterprises.com',
+// });
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://nabiya-enterprises.com',
+  site: 'https://rashidnsit.github.io/Nabiya-Enterprises-Arif-Bhai-Saket/',
+  base: '/Nabiya-Enterprises-Arif-Bhai-Saket/',
 });
